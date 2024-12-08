a8e7eba98fb49acb5c6e539ac067655f62e1b933
Alexandre Shimono <alexandre@shimono.com.br>
2024-12-08 16:46:31 -0300

    apply corrections suggested by sourcery

--- steam_scrapper/errors.py
+++ steam_scrapper/errors.py
@@ -9,3 +9,12 @@ class SteamResourceNotAvailable(Exception):
     """
     The requested Steam resource was not available or was not found.
     """
+    pass
+
+
+class DatabaseDeletionError(Exception):
+    """
+    There was a problem deleting entries from the Database.
+    """
+
+    pass
